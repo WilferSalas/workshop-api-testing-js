9415c25b0121c2d0fe12956c4cc034c37f3a2a1f
TEAM\Wilfer.Salas <Wilfer Salas>
2021-07-06 09:56:32 -0500

    Change mocha for Jest

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -1,15 +1,18 @@
 const agent = require('superagent');
 const statusCode = require('http-status-codes');
-const chai = require('chai');
 
-const expect = chai.expect;
+import 'regenerator-runtime/runtime'
 
 describe('First Api Tests', () => {
   it('Consume GET Service', async () => {
     const response = await agent.get('https://httpbin.org/ip');
 
-    expect(response.status).to.equal(statusCode.OK);
-    expect(response.body).to.have.property('origin');
+    expect(response).toMatchObject({
+      status: statusCode.OK,
+      body: expect.objectContaining({
+        origin: expect.any(String)
+      })
+    });
   });
 
   it('Consume GET Service with query parameters', async () => {
@@ -21,35 +24,55 @@ describe('First Api Tests', () => {
 
     const response = await agent.get('https://httpbin.org/get').query(query);
 
-    expect(response.status).to.equal(statusCode.OK);
-    expect(response.body.args).to.eql(query);
+    expect(response).toMatchObject({
+      status: statusCode.OK,
+      body: expect.objectContaining({
+        args: query
+      })
+    });
   });
 
   it('Consume HEAD Service', async () => {
     const response = await agent.get('https://httpbin.org/response-headers?freeform=');
 
-    expect(response.status).to.equal(statusCode.OK);
-    expect(response.body).to.have.property('Content-Length');
+    expect(response).toMatchObject({
+      status: statusCode.OK,
+      body: expect.objectContaining({
+        'Content-Length': expect.any(String)
+      })
+    });
   });
 
   it('Consume PATCH Service', async () => {
     const response = await agent.patch('https://httpbin.org/patch');
 
-    expect(response.status).to.equal(statusCode.OK);
-    expect(response.body).to.have.property('origin');
+    expect(response).toMatchObject({
+      status: statusCode.OK,
+      body: expect.objectContaining({
+        origin: expect.any(String)
+      })
+    });
   });
 
   it('Consume PUT Service', async () => {
     const response = await agent.put('https://httpbin.org/put');
 
-    expect(response.status).to.equal(statusCode.OK);
-    expect(response.body).to.have.property('origin');
+    expect(response).toMatchObject({
+      status: statusCode.OK,
+      body: expect.objectContaining({
+        origin: expect.any(String)
+      })
+    });
   });
 
   it('Consume DELETE Service', async () => {
     const response = await agent.delete('https://httpbin.org/delete');
 
-    expect(response.status).to.equal(statusCode.OK);
-    expect(response.body).to.have.property('origin');
+    expect(response).toMatchObject({
+      status: statusCode.OK,
+      body: expect.objectContaining({
+        origin: expect.any(String)
+      })
+    });
   });
 });
